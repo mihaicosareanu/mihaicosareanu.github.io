52decff9c5ab42cf3cbee4e276c587d615114af6
Mihai Cosareanu <mihai.cosareanu@gmail.com>
2017-09-30 18:06:55 +0300

    Add unite gallery and google analytics

--- assets/index.js
+++ assets/index.js
@@ -7,9 +7,18 @@ $(document).ready(function () {
             slider.removeClass('loading');
         }
     });
-});
 
-$('.mosaicgallery').mosaicflow({
-  itemSelector: '.mosaicflow__item',
-  minItemWidth: 300
+    var galleries = [];
+    $(".gallery").each(function(i, obj) {
+        galleries.push(obj);
+        $(obj).attr('id',   this.id + '_' + i);
+    });
+
+    galleries.forEach(function(obj) {
+        $(obj).unitegallery({
+            tiles_type:"justified",
+            tile_enable_textpanel: true,
+            tile_textpanel_source: "alt"
+        });
+    }); 
 });
